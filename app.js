b95f4e003410aa0cdf2c28458c2c181081a9bd1e
Bhannasa <adityabhan125@gmail.com>
2022-10-13 09:11:55 +0530

    commit 1

--- app.js
+++ app.js
@@ -307,8 +307,8 @@ app.post("/like", function (req, res) {
 });
 
 
-
-app.listen(process.env.PORT, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
   console.log("server is running on port 3000");
 });
 
